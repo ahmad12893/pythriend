8978c8659412a2bf76c26a28540aae202386960f
Ahmad Ali <ahmad12893@hotmail.com>
2023-08-25 20:07:35 +0100

    small changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import wolframalpha
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
-activationSentence = "Hey Pythriend"
+activationSentence = "Hey"
 
 def speak(text, rate = 150):
     engine.setProperty('rate', rate)
@@ -18,10 +18,10 @@ def speak(text, rate = 150):
 
 def parseCommand():
     listener = sr.Recognizer()
-    print("How can I help, Ali?")
+    print("How can I help?")
 
     with sr.Microphone() as source:
-        listener.pause_threshold = 3
+        listener.pause_threshold = 2
         input_speech = listener.listen(source)
 
         try:
@@ -33,6 +33,84 @@ def parseCommand():
             speak("Say that again please...")
             print("Exception: " + str(exception))
             return "None"
+
         return query
 
+if __name__ == '__main__':
+    speak('Initializing Pythriend...')
 
+    while True:
+        query = parseCommand().lower().split()
+
+        if query[0] == activationSentence:
+            query.pop(0)
+
+            if query[0]== "say":
+                if 'hello' in query:
+                    speak("Hello, how are you?")
+                else:
+                    query.pop(0)
+                    speech = ' '.join(query)
+                    speak(speech)
+
+        if activationSentence.lower() in query:
+            print("Activation Sentence Detected")
+            speak("Yes, friend?")
+            query = parseCommand().lower()
+
+            if 'wikipedia' in query:
+                speak('Searching Wikipedia...')
+                query = query.replace("wikipedia", "")
+                results = wikipedia.summary(query, sentences=2)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
+
+            elif 'open youtube' in query:
+                speak('Opening Youtube...')
+                webbrowser.open("youtube.com")
+
+            elif 'open google' in query:
+                speak('Opening Google...')
+                webbrowser.open("google.com")
+
+            elif 'open stackoverflow' in query:
+                speak('Opening Stackoverflow...')
+                webbrowser.open("stackoverflow.com")
+
+            elif 'what time is it' in query:
+                strTime = datetime.now().strftime("%H:%M:%S")
+                speak(f"It is {strTime}")
+
+            elif 'open code' in query:
+                speak('Opening Visual Studio Code...')
+                codePath = "C:\\Users\\Ali\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+                webbrowser.open(codePath)
+
+            elif 'open spotify' in query:
+                speak('Opening Spotify...')
+                codePath = "C:\\Users\\Ali\\AppData\\Roaming\\Spotify\\Spotify.exe"
+                webbrowser.open(codePath)
+
+            elif 'open steam' in query:
+                speak('Opening Steam...')
+                codePath = "C:\\Program Files (x86)\\Steam\\Steam.exe"
+                webbrowser.open(codePath)
+
+            elif 'open epic games' in query:
+                speak('Opening Epic Games...')
+                codePath = "C:\\Program Files (x86)\\Epic Games\\Launcher\\Portal\\Binaries\\Win32\\EpicGamesLauncher.exe"
+                webbrowser.open(codePath)
+
+            elif 'open origin' in query:
+                speak('Opening Origin...')
+                codePath = "C:\\Program Files (x86)\\Origin\\Origin.exe"
+                webbrowser.open(codePath)
+
+            elif 'open uplay' in query:
+                speak('Opening Uplay...')
+                codePath = "C:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\Uplay.exe"
+                webbrowser.open(codePath)
+
+            elif 'open battle net' in query:
+                speak('Opening Battle Net...')
